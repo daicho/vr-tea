c22ee57653b284e264bc14883d95b1a267d4be7e
kumokumo1653 <fjsda526hdbs@gmail.com>
2020-06-09 21:33:57 +0900

    物理演算で試した

---
+++ tyaProject/Assets/Scripts/TrackTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public OVRSkeleton skeleton;
+    public GameObject cube;
+    void Start()
+    {
+        for(int i = 0; i < skeleton.Bones.Count;i++) {
+            Debug.Log("asdf");
+            GameObject temp =  Instantiate(cube);
+            temp.transform.position = skeleton.Bones[i].Transform.position;
+            temp.transform.parent = skeleton.Bones[i].Transform;
+        }   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+}
